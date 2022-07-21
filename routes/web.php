7fc15fad07230f7a00bf9e1c549185b7d48a6fcd
Deuti Gurung <deuti@ininepal.com>
2022-07-21 14:29:45 +0545

    fbsdk testing

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::group(['prefix' => 'login', 'middleware' => 'auth'], function () {
+    Route::get('/facebook', [\App\Http\Controllers\SocialController::class, 'redirectToProvider']);
+    Route::get('/callback', [\App\Http\Controllers\SocialController::class, 'handleProviderCallback']);
+});
+
 Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/posts', [\App\Http\Livewire\Posts::class, 'index'])->name('posts.index');
 Route::get('/users', [\App\Http\Livewire\Users::class, 'index'])->name('users.index');
+
+
