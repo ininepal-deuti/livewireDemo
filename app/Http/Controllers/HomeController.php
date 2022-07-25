7d818c7a0145a8d4d4f018414cf402e2579e982f
Deuti Gurung <deuti@ininepal.com>
2022-07-25 15:29:43 +0545

    artisan cmd create and email queue is wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendEmail;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +26,13 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function sendEmail()
+    {
+        $user  = User::find(2)->toArray();
+        //dd($user);
+        SendEmail::dispatch($user)->delay(now()->addMinutes(1));
+
     }
 }
